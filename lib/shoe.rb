b93ee8afe0293ecc35085122859f9307e0a72d04
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-10 14:23:12 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@ class Shoe
 def initialize(brand)
 end
 
-attr_accessor :@brand, :@color, :@size, :@material, :@condtion
-attr_reader :@brand
+attr_accessor :brand, :color, :size, :material, :condtion
+attr_reader :brand
 
 end 
